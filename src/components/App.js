3af4d23ef6e8aeb9926552dddd1da6084758eb7d
RK <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2024-07-31 05:03:19 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ItemList from './ItemList';
+import ItemDetail from './ItemDetail';
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<ItemList />} />
+        <Route path="/item/:id" element={<ItemDetail />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ItemDetail.jsx
@@ -0,0 +1,27 @@
+// src/components/ItemDetail.js
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const items = [
+  { id: '1', name: 'Item 1', description: 'Description of Item 1' },
+  { id: '2', name: 'Item 2', description: 'Description of Item 2' },
+  { id: '3', name: 'Item 3', description: 'Description of Item 3' },
+];
+
+function ItemDetail() {
+  const { id } = useParams();
+  const item = items.find(item => item.id === id);
+
+  if (!item) {
+    return <h2>Item not found</h2>;
+  }
+
+  return (
+    <div>
+      <h1>{item.name}</h1>
+      <p>{item.description}</p>
+    </div>
+  );
+}
+
+export default ItemDetail;
