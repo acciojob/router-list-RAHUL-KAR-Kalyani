2745a492aaeb947d664b171d7191e698b24fcbcb
RK <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2024-07-31 05:06:27 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,14 @@ import ItemList from './ItemList';
 import ItemDetail from './ItemDetail';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ItemList />} />
-        <Route path="/item/:id" element={<ItemDetail />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<ItemList />} />
+				<Route path="/item/:id" element={<ItemDetail />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
